c5d43666a5403f7ddd22fb6795ed3849d55bec35
samuelcromwell <cromwellsamuel3@gmail.com>
2024-01-27 20:19:57 +0300

    third commit

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('aboutus/', views.aboutus, name="aboutus"),
     path('login/', views.login, name="login"),
+    path('courses/', views.courses, name="courses"),
+    path('contactus/', views.login, name="contactus"),
+    path('signup/', views.signup, name="signup"),
+
 ]
--- users/views.py
+++ users/views.py
@@ -7,6 +7,12 @@ def aboutus(request):
     return render(request, 'users/aboutus.html')
 def login(request):
     return render(request, 'users/login.html')
+def contactus(request):
+    return render(request, 'users/contactus.html')
+def courses(request):
+    return render(request, 'users/courses.html')
+def signup(request):
+    return render(request, 'users/signup.html')
 
 
 # Create your views here.
